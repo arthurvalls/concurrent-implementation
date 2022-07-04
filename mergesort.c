ac34c3d2b0d5d41ac455b33e97dadb8df6e49438
Arthur Valls <73508310+arthurvalls@users.noreply.github.com>
2022-07-04 14:01:44 -0300

    update mergesort

--- mergesort.c
+++ mergesort.c
@@ -13,13 +13,16 @@ int *vetSeq; //vetor sequencial a ser ordenado
 int *vetConc; //vetor concorrente a ser ordenado
 
 //funcao merge padrao, utilizada para unir os vetores menores já ordenados
-void merge(int vetor[], int comeco, int meio, int fim){
+void merge(int *vetor, int comeco, int meio, int fim){
     int i, j, k; //variaveis contadoras
     int metade1 = meio - comeco + 1;
     int metade2 = fim - meio;
  
     /* vetores temporarios */
-    int E[metade1], D[metade2];
+    int *E, *D;
+    E = (int *) malloc(sizeof(int) * metade1);
+    D = (int *) malloc(sizeof(int) * metade2);
+
  
     /* copia as duas metades do vetor original para seus respectivos vetores temporarios */
     for (i = 0; i < metade1; i++)
@@ -52,10 +55,12 @@ void merge(int vetor[], int comeco, int meio, int fim){
         j++;
         k++;
     }
+    free(E);
+    free(D);
 }
 
 //mergesort recursivo padrao 
-void mergeSort(int vetor[], int comeco, int fim){
+void mergeSort(int * vetor, int comeco, int fim){
     if (comeco < fim) {
         int meio = comeco + (fim - comeco) / 2;
         // ordena primeira e seguda metade do vetor
@@ -67,7 +72,7 @@ void mergeSort(int vetor[], int comeco, int fim){
 }
 
 //funcao responsavel por juntar os subvetores ordenados por cada thread, ordenando o vetor original que ja possui sessoes ordenadas
-void merge_global(int vet[], int nthread, int a){    //o valor inteiro "a" delimita o periodo do merge em questão.
+void merge_global(int *vet, int nthread, int a){    //o valor inteiro "a" delimita o periodo do merge em questão.
     for (int i = 0; i < nthread; i = i + 2){
         int comeco = i * (bloco*a);              //a multiplica bloco, que é o número de elementos que cada thread irá ordenar
         int fim = ((i + 2) * bloco*a) - 1;
@@ -83,7 +88,7 @@ void merge_global(int vet[], int nthread, int a){    //o valor inteiro "a" delim
 }
 
 //teste de corretude
-void teste_ordem(int vet[]){
+void teste_ordem(int *vet){
     for (int i = 1; i < nthread; i++){
         if (vet[i] < vet[i - 1]){
             printf("Error. Fora de ordem: valor %d \n", vet[i]);
@@ -93,7 +98,7 @@ void teste_ordem(int vet[]){
 }
 
 //teste de igualdade entre os vetores sequencial e concorrente
-void teste_igualdade(int vet1[], int vet2[]){
+void teste_igualdade(int *vet1, int *vet2){
     for(int i=0; i<dim; i++){
         if(vet1[i] != vet2[i]){
             printf("ERRO--Vetores diferentes");
@@ -120,6 +125,8 @@ void *thread_merge_sort(void *arg){
 }
 
 int main(int argc, const char *argv[]){
+    double inicio, fim, tempSeq, tempConc;
+
     if (argc < 3){
         fprintf(stderr, "Digite: %s <dimensao do vetor> <numero threads>\n", argv[0]);
         return 3;
@@ -128,8 +135,6 @@ int main(int argc, const char *argv[]){
     nthread = atoi(argv[2]);
     bloco = dim/nthread;
     desvio = dim % nthread;      //resto da divisao
-    double inicio, fim, tempSeq, tempConc;
-
     //aloca os vetores 
     vetConc = (int *) malloc(sizeof(int) * dim);
     vetSeq = (int *) malloc(sizeof(int) * dim);
@@ -143,6 +148,7 @@ int main(int argc, const char *argv[]){
     //execucao sequencial
     GET_TIME(inicio);
     mergeSort(vetSeq, 0, dim-1);
+
     teste_ordem(vetSeq);
     
     //tempo sequencial
@@ -180,4 +186,9 @@ int main(int argc, const char *argv[]){
     //teste de corretude
     teste_ordem(vetConc);
     teste_igualdade(vetSeq, vetConc);
+
+    free(vetConc);
+    free(vetSeq);
+
+
 }
